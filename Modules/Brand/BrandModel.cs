3ccee7803ce9706691aa34d828f41b9c52bd0f41
Hamza Zeryouh <zeryouhbusiness@gmail.com>
2023-06-09 17:43:30 +0200

    create  models

---
+++ Modules/Brand/BrandModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CarWebAPI.Modules
+{
+    public class BrandModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Modules/Parametre/ModelView.cs
@@ -0,0 +1,8 @@
+﻿namespace CarWebAPI.Modules.Parametre
+{
+    public class ModelView
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
