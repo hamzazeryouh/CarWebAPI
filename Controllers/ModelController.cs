5659513b791dbe22a70abf53a1c9eda6c6d44311
Hamza Zeryouh <zeryouhbusiness@gmail.com>
2023-06-09 17:42:24 +0200

    add controller model

---
+++ Controllers/ModelController.cs
@@ -0,0 +1,17 @@
+﻿using CarWebAPI.Entities.Domain;
+using CarWebAPI.Entities.Domain.Parametre;
+using CarWebAPI.Modules;
+using CarWebAPI.Modules.Parametre;
+using CarWebAPI.Services.BaseService;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/Model")]
+[ApiController]
+public class ModelController : BaseController<Model, ModelView>
+{
+    public ModelController(IBaseService<Model, ModelView> service) : base(service)
+    {
+    }
+}
+
+
