203e232d4fa02720b66557fc7d0ce15edd058c97
Hamza Zeryouh <zeryouhbusiness@gmail.com>
2023-06-09 17:43:56 +0200

    add service model

---
+++ Services/ModelService/IModelService.cs
@@ -0,0 +1,13 @@
+﻿using CarWebAPI.Entities.Domain;
+using CarWebAPI.Entities.Domain.Parametre;
+using CarWebAPI.Modules;
+using CarWebAPI.Modules.Parametre;
+using CarWebAPI.Services.BaseService;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace CarWebAPI.Services
+{
+    public interface IModelService:IBaseService<Entities.Domain.Parametre.Model, ModelView>
+    {
+    }
+}
