66b2b11f76bbc9f9cdc3d105df3f9a4ce28e45c1
Hamza Zeryouh <zeryouhbusiness@gmail.com>
2023-06-09 17:42:34 +0200

    add repo brand

---
+++ Data/Repositores/BrandRepository/BrandRepository.cs
@@ -0,0 +1,12 @@
+﻿using CarWebAPI.Data.Repositores.BaseRepsitory;
+using CarWebAPI.Entities.Domain.Parametre;
+
+namespace CarWebAPI.Data.Repositores.BrandRepository
+{
+    public class BrandRepository : Repository<Brand>, lBrandRepository
+    {
+        public BrandRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
