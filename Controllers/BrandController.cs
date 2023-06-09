74870d1be13fd777913917c3bcdf39818dd7ee44
Hamza Zeryouh <zeryouhbusiness@gmail.com>
2023-06-09 17:42:16 +0200

    add controller brand

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,16 @@
+﻿using CarWebAPI.Entities.Domain;
+using CarWebAPI.Entities.Domain.Parametre;
+using CarWebAPI.Modules;
+using CarWebAPI.Services.BaseService;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/Brand")]
+[ApiController]
+public class BrandController : BaseController<Brand, BrandModel>
+{
+    public BrandController(IBaseService<Brand, BrandModel> service) : base(service)
+    {
+    }
+}
+
+
