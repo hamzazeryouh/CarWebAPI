becd651ae5e4fded08298d005d8497b18d3d6106
Hamza Zeryouh <zeryouhbusiness@gmail.com>
2023-06-10 23:29:45 +0200

    AddFeature Service

---
+++ Services/FeatureService/FeatureService.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CarWebAPI.Data.Repositores.BaseRepsitory;
+using CarWebAPI.Entities.Domain.Parametre;
+using CarWebAPI.Modules.Parametre;
+using CarWebAPI.Services.BaseService;
+
+namespace CarWebAPI.Services.FeatureService
+{
+    public class FeatureService:BaseService<Feature,FeatureModel>,IFeatureService
+    {
+        public FeatureService(IRepository<Feature> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+    }
+}
---
+++ Services/FeatureService/IFeatureService.cs
@@ -0,0 +1,10 @@
+﻿using CarWebAPI.Entities.Domain.Parametre;
+using CarWebAPI.Modules.Parametre;
+using CarWebAPI.Services.BaseService;
+
+namespace CarWebAPI.Services.FeatureService
+{
+    public interface IFeatureService:IBaseService<Feature,FeatureModel>
+    {
+    }
+}
