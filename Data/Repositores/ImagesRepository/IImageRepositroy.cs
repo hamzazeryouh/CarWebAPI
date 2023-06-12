0f39c18e5d0e3d3ecea5ddbc1b309423e398be3d
Hamza Zeryouh <zeryouhbusiness@gmail.com>
2023-06-12 10:59:14 +0200

    add Images Repo

---
+++ Data/Repositores/ImagesRepository/IImageRepositroy.cs
@@ -0,0 +1,9 @@
+﻿using CarWebAPI.Data.Repositores.BaseRepsitory;
+using CarWebAPI.Entities.Domain;
+
+namespace CarWebAPI.Data.Repositores.ImagesRepository
+{
+    public interface IImageRepositroy:IRepository<Images>
+    {
+    }
+}
---
+++ Data/Repositores/ImagesRepository/ImageRepository.cs
@@ -0,0 +1,12 @@
+﻿using CarWebAPI.Data.Repositores.BaseRepsitory;
+using CarWebAPI.Entities.Domain;
+
+namespace CarWebAPI.Data.Repositores.ImagesRepository
+{
+    public class ImageRepository : Repository<Images>, IImageRepositroy
+    {
+        public ImageRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
