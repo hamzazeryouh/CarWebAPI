c1912ae2471156ade4346a948da6a18eeb182dfd
Hamza Zeryouh <zeryouhbusiness@gmail.com>
2023-06-12 10:46:17 +0200

    transmission repo

---
+++ Data/Repositores/TransmissionRepository/TransmissionRepository.cs
@@ -0,0 +1,12 @@
+﻿using CarWebAPI.Data.Repositores.BaseRepsitory;
+using CarWebAPI.Entities.Domain.Parametre;
+
+namespace CarWebAPI.Data.Repositores.TransmissionRepository
+{
+    public class TransmissionRepository : Repository<Transmission>
+    {
+        public TransmissionRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
